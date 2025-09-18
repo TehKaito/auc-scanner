1863661202daf42e16363247252aedb5d3c61584
kofman <kofman@banki.ru>
2025-09-18 18:02:53 +0300

    Pew-Pew 4

--- AlchemyHelper.lua
+++ AlchemyHelper.lua
@@ -1,37 +1,70 @@
--- Создаем глобальную таблицу аддона
+-- AlchemyHelper.lua
+
+-- Глобальная таблица для аддона
 AlchemyHelper = {}
 
+-------------------------------------------------
 -- Функция переключения окна
+-------------------------------------------------
 function AlchemyHelper:ToggleUI()
-    if AlchemyUI:IsShown() then
-        AlchemyUI:Hide()
+    if AlchemyUI then
+        if AlchemyUI:IsShown() then
+            AlchemyUI:Hide()
+        else
+            AlchemyUI:Show()
+        end
     else
-        AlchemyUI:Show()
+        print("|cffff0000AlchemyHelper: UI не загружен.|r")
     end
 end
 
--- Создание кнопки у миникарты
+-------------------------------------------------
+-- Команда чата
+-------------------------------------------------
+SLASH_ALCHEMY1 = "/alchemy"
+SlashCmdList["ALCHEMY"] = function()
+    AlchemyHelper:ToggleUI()
+end
+
+-------------------------------------------------
+-- Кнопка у миникарты (для стандартного UI)
+-------------------------------------------------
 local button = CreateFrame("Button", "AlchemyHelperMinimapButton", Minimap)
 button:SetSize(32, 32)
 button:SetFrameStrata("MEDIUM")
 button:SetFrameLevel(8)
-button:SetHighlightTexture("Interface\\Minimap\\UI-Minimap-ZoomButton-Highlight")
 
--- Иконка
+-- Иконка на кнопке
 local icon = button:CreateTexture(nil, "BACKGROUND")
-icon:SetTexture("Interface\\Icons\\inv_potion_01")
+icon:SetTexture("Interface\\Icons\\inv_potion_01") -- можно заменить на любую
 icon:SetSize(20, 20)
 icon:SetPoint("CENTER")
 
--- Позиция кнопки (рядом с миникартой)
+-- Позиция у миникарты
 button:SetPoint("TOPLEFT", Minimap, "TOPLEFT")
 
 -- Скрипты
-button:SetScript("OnClick", function() AlchemyHelper:ToggleUI() end)
+button:SetScript("OnClick", function()
+    AlchemyHelper:ToggleUI()
+end)
+
 button:SetScript("OnEnter", function(self)
     GameTooltip:SetOwner(self, "ANCHOR_LEFT")
     GameTooltip:AddLine("Alchemy Helper")
-    GameTooltip:AddLine("Кликните, чтобы открыть/закрыть окно", 1, 1, 1)
+    GameTooltip:AddLine("Клик: открыть/закрыть окно", 1, 1, 1)
+    GameTooltip:AddLine("/alchemy - открыть из чата", 0.8, 0.8, 0.8)
     GameTooltip:Show()
 end)
-button:SetScript("OnLeave", function() GameTooltip:Hide() end)
+
+button:SetScript("OnLeave", function()
+    GameTooltip:Hide()
+end)
+
+-------------------------------------------------
+-- Сообщение при загрузке
+-------------------------------------------------
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_LOGIN")
+f:SetScript("OnEvent", function()
+    print("|cff00ff00AlchemyHelper загружен.|r Напиши |cffffff00/alchemy|r чтобы открыть окно.")
+end)
