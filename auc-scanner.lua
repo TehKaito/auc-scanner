ae8688f00f3caff2d25e1e7cc8fc279b7c2a2267
kofman <kofman@banki.ru>
2025-09-18 19:09:01 +0300

    Pew-Pew 6

--- auc-scanner.lua
+++ auc-scanner.lua
@@ -1,20 +1,25 @@
--- Создаём простое окно
-local f = CreateFrame("Frame", "AucScannerFrame", UIParent, "BasicFrameTemplate")
-f:SetSize(300, 200)
-f:SetPoint("CENTER")
-f:Hide()
+local AucScannerUI = CreateFrame("Frame", "AucScannerMainFrame", UIParent, "BasicFrameTemplateWithInset")
+AucScannerUI:SetSize(300, 200)
+AucScannerUI:SetPoint("CENTER")
+AucScannerUI:SetMovable(true)
+AucScannerUI:EnableMouse(true)
+AucScannerUI:RegisterForDrag("LeftButton")
+AucScannerUI:SetScript("OnDragStart", AucScannerUI.StartMoving)
+AucScannerUI:SetScript("OnDragStop", AucScannerUI.StopMovingOrSizing)
+AucScannerUI:Hide()
 
--- Заголовок окна
-f.title = f:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-f.title:SetPoint("TOP", f, "TOP", 0, -10)
-f.title:SetText("Auc Scanner")
+-- Заголовок
+AucScannerUI.title = AucScannerUI:CreateFontString(nil, "OVERLAY")
+AucScannerUI.title:SetFontObject("GameFontHighlight")
+AucScannerUI.title:SetPoint("LEFT", AucScannerUI.TitleBg, "LEFT", 5, 0)
+AucScannerUI.title:SetText("Auc Scanner")
 
--- Регистрируем slash-команду /aucs
+-- Слеш-команда
 SLASH_AUCSCANNER1 = "/aucs"
 SlashCmdList["AUCSCANNER"] = function()
-    if f:IsShown() then
-        f:Hide()
+    if AucScannerUI:IsShown() then
+        AucScannerUI:Hide()
     else
-        f:Show()
+        AucScannerUI:Show()
     end
 end
