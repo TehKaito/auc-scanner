0d197b7112e0b88120b71dc2cee2e9cc4a8182a9
kofman <kofman@banki.ru>
2025-09-18 20:04:02 +0300

    Pew-Pew

--- auc-scanner.lua
+++ auc-scanner.lua
@@ -1,65 +1,55 @@
--- /aucs показать/скрыть окно
-local function AucScanner_Toggle()
-    if AucScannerFrame:IsShown() then
-        AucScannerFrame:Hide()
-    else
-        AucScannerFrame:Show()
+local function DrawPeacebloom()
+    local itemID = 2447 -- Peacebloom
+    local name, _, _, _, _, _, _, _, _, icon = GetItemInfo(itemID)
+
+    if not name then
+        name = "Loading..."
+        icon = "Interface\\Icons\\INV_Misc_QuestionMark"
     end
+
+    local tex = AucScannerFrame:CreateTexture(nil, "ARTWORK")
+    tex:SetSize(32, 32)
+    tex:SetPoint("TOPLEFT", 20, -40)
+    tex:SetTexture(icon)
+
+    local label = AucScannerFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    label:SetPoint("LEFT", tex, "RIGHT", 5, 0)
+    label:SetText(name)
+
+    -- сохраним, чтобы обновить позже
+    AucScannerFrame._peacebloom = { tex = tex, label = label, id = itemID }
 end
 
--- перетаскивание
-function AucScanner_OnDragStart() AucScannerFrame:StartMoving() end
-function AucScanner_OnDragStop()  AucScannerFrame:StopMovingOrSizing() end
-
--- один элемент (твой формат: [id] = { name = ... })
-local line
-
-local function DrawOnce()
-    if line then return end
-
-    local itemID = 2447
-    local data   = AlchemyIngredients and AlchemyIngredients[itemID]
-    local name   = (data and data.name) or ("Item "..itemID)
-
-    -- иконка: сначала ставим вопросик, потом обновим, когда клиент подгрузит информацию
-    local icon = select(10, GetItemInfo(itemID)) or "Interface\\Icons\\INV_Misc_QuestionMark"
-
-    line = {}
-    line.icon = AucScannerFrame:CreateTexture(nil, "ARTWORK")
-    line.icon:SetSize(32, 32)
-    line.icon:SetPoint("TOPLEFT", 20, -40)
-    line.icon:SetTexture(icon)
-
-    line.label = AucScannerFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-    line.label:SetPoint("LEFT", line.icon, "RIGHT", 5, 0)
-    line.label:SetText(name)
-end
-
-local function RefreshIconIfReady()
-    if not line then return end
-    local icon = select(10, GetItemInfo(2447))
-    if icon then line.icon:SetTexture(icon) end
+local function RefreshIcons()
+    if not AucScannerFrame._peacebloom then return end
+    local name, _, _, _, _, _, _, _, _, icon = GetItemInfo(AucScannerFrame._peacebloom.id)
+    if name and icon then
+        AucScannerFrame._peacebloom.tex:SetTexture(icon)
+        AucScannerFrame._peacebloom.label:SetText(name)
+    end
 end
 
 local function OnEvent(self, event, ...)
     if event == "GET_ITEM_INFO_RECEIVED" then
-        RefreshIconIfReady()
+        RefreshIcons()
     end
 end
 
 local function AucScanner_OnLoad()
     SLASH_AUCSCANNER1 = "/aucs"
-    SlashCmdList["AUCSCANNER"] = AucScanner_Toggle
+    SlashCmdList["AUCSCANNER"] = function()
+        if AucScannerFrame:IsShown() then
+            AucScannerFrame:Hide()
+        else
+            AucScannerFrame:Show()
+            DrawPeacebloom()
+        end
+    end
 
-    -- перерисовка, когда окно показывают
-    AucScannerFrame:SetScript("OnShow", DrawOnce)
-
-    -- когда иконка подгрузится из кеша клиента
     AucScannerFrame:RegisterEvent("GET_ITEM_INFO_RECEIVED")
     AucScannerFrame:SetScript("OnEvent", OnEvent)
 end
 
--- ждём логина
 local loader = CreateFrame("Frame")
 loader:RegisterEvent("PLAYER_LOGIN")
 loader:SetScript("OnEvent", AucScanner_OnLoad)
